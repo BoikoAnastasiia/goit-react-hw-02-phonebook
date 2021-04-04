9e9b07b46b8578b264cd1068b4824dcba81bdf6c
anastasyah86 <nastasyah86@gmail.com>
2021-04-04 07:49:58 +0300

    added  phone imput

--- src/Components/PhoneBookForm/PhoneBookForm.jsx
+++ src/Components/PhoneBookForm/PhoneBookForm.jsx
@@ -14,11 +14,6 @@ class PhoneBookForm extends Component {
     this.reset();
   };
 
-  handleChange = event => {
-    const { name, contacts } = event.currentTarget;
-    this.setState({ [contacts]: [{ name, id: uuidv4() }] });
-  };
-
   reset = () => {
     this.setState({ name: '' });
   };
@@ -34,6 +29,16 @@ class PhoneBookForm extends Component {
           name="name"
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+          required
+        />
+        <label className={styles.label}> Phone number </label>
+
+        <input
+          type="tel"
+          className={styles.input}
+          name="number"
+          pattern="(\+?( |-|\.)?\d{1,2}( |-|\.)?)?(\(?\d{3}\)?|\d{3})( |-|\.)?(\d{3}( |-|\.)?\d{4})"
+          title="Номер телефона должен состоять из 11-12 цифр и может содержать цифры, пробелы, тире, пузатые скобки и может начинаться с +"
           required
         />
         <button onSubmit={this.handleSubmit} className={styles.button}>
